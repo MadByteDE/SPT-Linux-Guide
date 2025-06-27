bfe98547a9bae972160a1ff34034b2b0e976aa65
MadByte <madbyte@tuta.io>
2025-06-27 23:29:19 +0200

    server-pre-launch-script: convert tabs to spaces

--- scripts/server.sh
+++ scripts/server.sh
@@ -7,8 +7,8 @@ readonly GAMEDIR="${GAMEDIR:-"${ROOT_PATH}"}"
 # NOTE: If a Flatpak version of Lutris is used, we still want to launch the native
 # server with the 'dotnet-runtime' and 'aspnet-runtime' packages from the host system.
 m_run() {
-	if [[ -z "${FLATPAK_SANDBOX_DIR}" ]]; then "$@"
-	else flatpak-spawn --host "$@"; fi
+    if [[ -z "${FLATPAK_SANDBOX_DIR}" ]]; then "$@"
+    else flatpak-spawn --host "$@"; fi
 }
 
 # Skip launch if another instance is already running
@@ -18,8 +18,8 @@ cd "${GAMEDIR}"
 
 TERMS=("foot" "alacritty" "ptyxis" "konsole" "gnome-terminal" "xfce4-terminal" "kgx" "kitty" "xterm")
 for term in "${TERMS[@]}"; do
-	if m_run command -v $term; then
-		m_run $term -e "./SPT.Server.Linux"
-		break
-	fi
+    if m_run command -v $term; then
+        m_run $term -e "./SPT.Server.Linux"
+        break
+    fi
 done
