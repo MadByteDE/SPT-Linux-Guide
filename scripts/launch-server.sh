727f1517c469da48891b19cddc7deef8990d7622
MadByte <madbyte@tuta.io>
2025-10-07 19:42:51 +0200

    server-script: v2025.10-1 - Fixed for the new SPT file structure - Code cleanup / minor formatting changes - bump version

--- scripts/launch-server.sh
+++ scripts/launch-server.sh
@@ -2,29 +2,26 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # #
 #       SPT - Server pre-launch script        #
-#                v2025.9-1                    #
+#                v2025.10-1                   #
 # # # # # # # # # # # # # # # # # # # # # # # #
 
-readonly ROOT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-readonly TERMINALS=("foot" "alacritty" "ptyxis" "kgx" "konsole" "gnome-terminal" "xfce4-terminal" "kitty" "xterm")
-GAMEDIR="${GAMEDIR:-"${ROOT_PATH}"}"
+ROOT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TERMINALS=( "alacritty" "foot" "ptyxis"  "kgx" "konsole" "gnome-terminal" "xfce4-terminal" "kitty" "xterm" )
 
-# NOTE: When Flatpak Lutris is used, we don't have access to `dotnet-runtime` and
-# `aspnet-runtime` packages on the host system. To work around this limitation,
+# NOTE: When Flatpak Lutris is used, we don't have access to `aspnetcore-runtime`
+# packages on the host system. To work around this limitation,
 # we use `flatpak-spawn --host` to launch the server outside of the sandboxed
-# environment.
+# environment. (not supported with some desktop portals on Wayland e.g. `cinnamon/xapp`)
 m_run() {
     if [[ -z "${FLATPAK_SANDBOX_DIR}" ]]; then "$@"
     else flatpak-spawn --host "$@"; fi
 }
 
 # Skip launch if another instance is already running
-[[ $(m_run pidof "SPT.Server.Linux") != "" ]] && exit 0
+[[ -n $( m_run pidof "SPT.Server.Linux" ) ]] && exit 0
 
-for terminal in "${TERMINALS[@]}"; do
-    if m_run command -v $terminal; then
-        cd "${GAMEDIR}"
-        m_run $terminal -e "./SPT.Server.Linux"
-        break
-    fi
+for term in "${TERMINALS[@]}"; do
+    if ! m_run command -v $term &>/dev/null; then continue; fi
+    cd "${ROOT_PATH}/SPT"
+    m_run $term -e "./SPT.Server.Linux"; break
 done
