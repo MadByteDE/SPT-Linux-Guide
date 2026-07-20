14fe7be9d2474e582c0f74a6d7cf49c3ff62cbfc
MadByte <madbyte@tuta.io>
2026-07-20 19:22:26 +0200

    scripts: Add launcher.sh + update server.sh

---
+++ scripts/launcher.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Set Proton version
+export PROTONPATH="${PROTONPATH:-GE-Proton}"
+
+# Set wine prefix
+export WINEPREFIX="${WINEPREFIX:-$HOME/Games/tarkov}"
+
+# Add additions runtime directory to path
+RUNTIME_PATH="${XDG_DATA_HOME:-${HOME}/.local/share}/spt-additions/runtime"
+[[ -d "${RUNTIME_PATH}" ]] && export PATH=$RUNTIME_PATH:$PATH
+
+# Get umu-run binary path
+UMU_PATH=$( command -v umu-run )
+
+if [[ -z "${UMU_PATH}" ]]; then
+	echo "Cannot find umu-run executable"
+	exit 1
+fi
+
+# Environment variables
+export PROTON_USE_XALIA=0
+export PROTON_MEDIA_USE_GST=1
+export DOTNET_ROOT=
+export DOTNET_BUNDLE_EXTRACT_BASE_DIR=
+
+# Launch
+cd "SPT" && "$UMU_PATH" "SPT.Launcher.exe"
---
+++ scripts/server.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Uncomment line below if .NET is installed in your HOME directory
+# export DOTNET_ROOT=$HOME/.dotnet
+
+TERMINALS=( "alacritty" "ghostty" "foot" "terminator" "ptyxis" "cosmic-terminal"
+            "kgx" "konsole" "gnome-terminal" "xfce4-terminal" "kitty" "xterm" )
+
+if [[ -n $( pidof "SPT.Server.Linux" ) ]]; then
+    echo "ERROR: Another instance of the server is already running"
+    exit 1
+fi
+
+for term in "${TERMINALS[@]}"; do
+    if ! command -v $term &>/dev/null; then continue; fi
+    cd "SPT" && $term -e "./SPT.Server.Linux"
+    exit
+done
+
+echo "ERROR: Failed to find a terminal emulator"
+exit 1
