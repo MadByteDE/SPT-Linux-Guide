fc0c9c62da8b0eee501b4791db2170e79e8735e3
MadByte <madbyte@tuta.io>
2026-07-22 16:29:11 +0200

    server.sh: Get root path to fix issues with Lutris pre-launch commands

--- scripts/server.sh
+++ scripts/server.sh
@@ -3,6 +3,7 @@
 # Uncomment line below if .NET is installed in your HOME directory
 # export DOTNET_ROOT=$HOME/.dotnet
 
+ROOT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
 TERMINALS=( "alacritty" "ghostty" "foot" "terminator" "ptyxis" "cosmic-terminal"
             "kgx" "konsole" "gnome-terminal" "xfce4-terminal" "kitty" "xterm" )
 
@@ -12,16 +13,14 @@ m_run() {
 }
 
 if [[ -n $( m_run pidof "SPT.Server.Linux" ) ]]; then
-    echo "ERROR: Another instance is already running"
-    exit 1
+    echo "ERROR: Another instance is already running"; exit 1
 fi
 
 for term in "${TERMINALS[@]}"; do
     if ! m_run command -v $term &>/dev/null; then continue; fi
-    cd "SPT" || exit 1
+    cd "${ROOT_PATH}/SPT" || exit 1
     m_run $term -e "./SPT.Server.Linux"
     exit
 done
 
-echo "ERROR: Failed to find a terminal emulator"
-exit 1
+echo "ERROR: Failed to find a terminal emulator"; exit 1
